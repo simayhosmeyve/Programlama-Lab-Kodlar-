b997a9d36e420e0262f6ec2efeaa9fef58f50373
simayhosmeyve <44275183+simayhosmeyve@users.noreply.github.com>
2020-03-02 21:39:29 +0300

    Add files via upload

---
+++ hafta2.py
@@ -0,0 +1,34 @@
+
+#Insertion Sort
+#Listede bir kısım sıralı kısım olarak ayrılıyor ve ekleme yaparak sıralı kısımda karşılaştırma yapıyor.
+def insertionSort(liste):
+    n = len(liste)
+    for i in range(1,n):
+        key = liste[i]
+        j = i-1
+        while j>=0 and key<liste[j]:    
+            j=j-1
+            liste[j+1]=liste[j]
+            liste[j+1]=key
+liste = [3,10,2,6,5,9]
+insertionSort(liste)
+print(liste)
+
+#Shell Sort
+#Atlama yaparak ve atlama sayısını yarıya indirerek sıralar
+def shellSort(liste):
+    n=len(liste)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = liste[i]
+            j = i
+            while j >= gap and liste[j - gap] > temp:
+                liste[j] = liste[j - gap]
+                j = j-gap
+        liste[j] = temp
+        gap //= 2
+liste = [3,10,2,6,5,9]
+shellSort(liste,len(liste))
+print(liste)
+
